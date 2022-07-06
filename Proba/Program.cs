c5eb7e830722be85f561a9004fc16c1a2fea00d9
FillKolo <107361266+FillKolo@users.noreply.github.com>
2022-07-06 14:41:16 +0300

    06.07.2022

--- Proba/Program.cs
+++ Proba/Program.cs
@@ -10,8 +10,20 @@ namespace Proba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("пошел нахуй");
-            Console.WriteLine("пошел нахуй2");
+
+            for (int i = 0; i < 10; i++) 
+            {
+                string msg = Console.ReadLine();
+                if (msg == "exit")
+                {
+                    break;
+                }
+
+                Console.WriteLine(i);
+            }   
+                
+
+            Console.ReadLine();
 
         }
     }

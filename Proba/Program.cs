4821e72eba3dabcdff853016fa52f7b7740f4495
Филипп Колотадзе <kolo.fill@gmail.com>
2022-08-04 16:12:46 +0300

    123321

--- Proba/Program.cs
+++ Proba/Program.cs
@@ -6,25 +6,55 @@ using System.Threading.Tasks;
 
 namespace Proba
 {
-    internal class Program
+
+    internal class Programnet
     {
         static void Main(string[] args)
         {
 
-            for (int i = 0; i < 10; i++) 
+            int [] myArray = new int[]
             {
-                string msg = Console.ReadLine();
-                if (msg == "exit")
+                1, 412, 4, 98, 76, 13, 14, 18, 32, 5
+            };
+
+            Array.Sort(myArray);
+
+            int EvenNumber = 0;
+            int OddNumber = 0;
+
+            foreach (int i in myArray)
+                if (i % 2 == 0)
                 {
-                    break;
+                    EvenNumber++;
+                }
+            else
+                {
+                    OddNumber++;  
                 }
 
-                Console.WriteLine(i);
-            }   
-                
+            int[] arrayEvenNumber = new int[EvenNumber];
+            int[] arrayOddNumber = new int[OddNumber];
+            int j = 0;
+            int k = 0;
 
-            Console.ReadLine();
-
+            foreach (var item in myArray)
+            {
+                if (item % 2 == 0)
+                {
+                    arrayEvenNumber[j] = item;
+                    j++;
+                }
+                else
+                {
+                    arrayOddNumber[k] = item;
+                    k++;
+                }
+            }
+            Console.WriteLine(arrayEvenNumber);
+            Console.WriteLine(arrayOddNumber);
         }
+
+
     }
+
 }

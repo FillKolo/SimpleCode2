6de51994a5e23c7fd2436e0f87416f23ee8697df
Филипп Колотадзе <kolo.fill@gmail.com>
2022-08-22 11:48:36 +0300

    petyx

--- Proba/Program.cs
+++ Proba/Program.cs
@@ -9,52 +9,33 @@ namespace Proba
 
     internal class Programnet
     {
+
         static void Main(string[] args)
         {
 
-            int [] myArray = new int[]
+            string myArray = Console.ReadLine();
+
+            string[] words = myArray.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            myArray = myArray.Replace("Петух", "петух");
+
+            string alexNazi =  "петух";
+
+            int countWorld = myArray.IndexOf(alexNazi);
+
+            if (countWorld >=0)
             {
-                1, 412, 4, 98, 76, 13, 14, 18, 32, 5
-            };
-
-            Array.Sort(myArray);
-
-            int EvenNumber = 0;
-            int OddNumber = 0;
-
-            foreach (int i in myArray)
-                if (i % 2 == 0)
-                {
-                    EvenNumber++;
-                }
-            else
-                {
-                    OddNumber++;  
-                }
-
-            int[] arrayEvenNumber = new int[EvenNumber];
-            int[] arrayOddNumber = new int[OddNumber];
-            int j = 0;
-            int k = 0;
-
-            foreach (var item in myArray)
-            {
-                if (item % 2 == 0)
-                {
-                    arrayEvenNumber[j] = item;
-                    j++;
-                }
-                else
-                {
-                    arrayOddNumber[k] = item;
-                    k++;
-                }
+                Console.WriteLine("Петух обнаружен");
             }
-            Console.WriteLine(arrayEvenNumber);
-            Console.WriteLine(arrayOddNumber);
+            else
+            {
+                Console.WriteLine("Петух деактивирован");
+            }
+            Console.ReadLine();
+
+            }
         }
 
 
     }
 
-}
+
